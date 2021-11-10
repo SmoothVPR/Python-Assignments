18c26be097fc9d5dd2c438de90e128289319b161
0xvpr <malikrb@live.com>
2021-11-10 14:34:33 -0600

    init

---
+++ nov9/assignment_1/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Author:   Malik R Booker
+Created:  November 9, 2021
+Modified: November 9, 2021
+"""
+
+ASSIGNMENT = "1"
+
+if __name__ == "__main__":
+    print(f"Assignment: {ASSIGNMENT}")
